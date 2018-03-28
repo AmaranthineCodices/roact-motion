2b56ce814e3a09fbb95b268a15b1656b7cc55911
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-03-28 19:41:20 -0400

    lowercase keys in springs; default values

--- src/spring.lua
+++ src/spring.lua
@@ -3,10 +3,13 @@
     Usage: spring(goal, stiffness, damping)
 ]]
 
+local DEFAULT_STIFFNESS = 170
+local DEFAULT_DAMPING = 26
+
 return function(value, stiffness, damping)
     return {
-        Value = value,
-        Stiffness = stiffness,
-        Damping = damping,
+        value = value,
+        stiffness = stiffness or DEFAULT_STIFFNESS,
+        damping = damping or DEFAULT_DAMPING,
     }
 end
