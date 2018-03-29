04639febf430a361c53b8fd05acac8195ad7a015
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-03-29 13:24:06 -0400

    add getValues utility function to SpringHelper

--- src/SpringHelper.lua
+++ src/SpringHelper.lua
@@ -4,6 +4,9 @@
 
 local SpringHelper = {}
 
+--[[
+    Gets a specific spring's value.
+]]
 function SpringHelper.getValue(value)
     -- Two possible cases:
     -- 1. number (instant-set, no animation). The value is just the value.
@@ -14,6 +17,19 @@ function SpringHelper.getValue(value)
     else
         return value.value
     end
+end
+
+--[[
+    Gets all the values of a table of springs.
+]]
+function SpringHelper.getValues(springs)
+    local values = {}
+
+    for key, spring in pairs(springs) do
+        values[key] = SpringHelper.getValue(spring)
+    end
+
+    return values
 end
 
 return SpringHelper
