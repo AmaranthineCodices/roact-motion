634b8580039c282bd90868567bc550b0332e808f
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-04-09 21:14:08 -0400

    warn if damping is less than or equal to 0; fixes #2

--- src/spring.lua
+++ src/spring.lua
@@ -6,6 +6,13 @@
 local Config = require(script.Parent.Config)
 
 return function(value, stiffness, damping)
+    if damping <= 0 then
+        warn(("damping is %f - this spring will oscillate indefinitely! Traceback:\n%s"):format(
+            damping,
+            debug.traceback()
+        ))
+    end
+
     return {
         value = value,
         stiffness = stiffness or Config.defaultStiffness,
